cf7a144cd5e4661e062e35ba4d3789b80e295766
annajonsson <149366579+annajonsson@users.noreply.github.com>
2026-04-27 09:16:20 +0200

    initial commit, no changes

---
+++ kodtestapi/src/main/java/com/kodtestannaj/kodtestapi/KodtestapiApplication.java
@@ -0,0 +1,13 @@
+package com.kodtestannaj.kodtestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodtestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KodtestapiApplication.class, args);
+	}
+
+}
---
+++ kodtestapi/src/test/java/com/kodtestannaj/kodtestapi/KodtestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodtestannaj.kodtestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodtestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
